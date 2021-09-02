a3bd7fa26bb54c55577d6a53bf3a4e34cb6345d1
ShimonIslam <shimon.ocean@gmail.com>
2021-09-02 14:54:49 -0700

    Sidebar Header and Search done, moving to implement firebase for chat feature

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,7 +1,8 @@
-import { Avatar, IconButton } from "@material-ui/core";
+import { Avatar, Button, IconButton } from "@material-ui/core";
 import styled from "styled-components";
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Sidebar() {
     return (
@@ -23,7 +24,12 @@ function Sidebar() {
 
             </Header>
 
+            <Search>
+                <SearchIcon />
+                <SearchInput />
+            </Search>
 
+            <SidebarButton>START A NEW CHAT</SidebarButton>
         </Container>
     )
 }
@@ -31,6 +37,28 @@ function Sidebar() {
 export default Sidebar
 
 const Container = styled.div`
+`;
+
+const Search = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    border-radius: 2px;
+`;
+
+const SearchInput = styled.input`
+    outline-width: 0;
+    border: none;
+    flex: 1;
+`;
+
+const SidebarButton = styled(Button)`
+    width: 100%;
+
+    &&& {
+        border-top: 1px solid whitesmoke;
+        border-bottom: 1px solid whitesmoke;
+    }
 `;
 
 const Header = styled.div`
