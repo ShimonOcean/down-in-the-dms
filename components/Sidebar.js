14d6b1f46b58673649ea98d3ff81f39c340c4260
ShimonIslam <shimon.ocean@gmail.com>
2021-09-01 21:36:40 -0700

    Sidebar header built

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,7 +1,29 @@
+import { Avatar, IconButton } from "@material-ui/core";
+import styled from "styled-components";
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
 function Sidebar() {
     return (
         <Container>
-            
+            <Header>
+                <UserAvatar />
+
+                <IconsContainer>
+                    {/* Turns icons into button */}
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+
+                </IconsContainer>
+
+            </Header>
+
+
         </Container>
     )
 }
@@ -9,5 +31,28 @@ function Sidebar() {
 export default Sidebar
 
 const Container = styled.div`
+`;
 
+const Header = styled.div`
+    display: flex;
+    position: sticky;
+    top: 0;
+    background-color: white;
+    z-index: 1;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px;
+    height: 80px;
+    border-bottom: 1px solid whitesmoke
+`;
+
+const UserAvatar = styled(Avatar)`
+    cursor: pointer;
+    
+    :hover {
+        opacity: 0.6;
+    }
+`;
+
+const IconsContainer = styled.div`
 `;
